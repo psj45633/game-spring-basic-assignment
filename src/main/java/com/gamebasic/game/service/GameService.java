74f6b7dc2ad054fc1242dda411b110f77ce7ac1e
psj45633 <psj45633@gmail.com>
2026-09-09 15:17:30 +0900

    fix : 예외처리 수정

--- src/main/java/com/gamebasic/game/service/GameService.java
+++ src/main/java/com/gamebasic/game/service/GameService.java
@@ -1,5 +1,6 @@
 package com.gamebasic.game.service;
 
+import com.gamebasic.common.exception.GameFinishedException;
 import com.gamebasic.common.exception.GameNotFoundException;
 import com.gamebasic.game.dto.*;
 import com.gamebasic.game.entity.Game;
@@ -62,7 +63,7 @@ public class GameService {
         Game game = findGame(gameId);
 
         if(game.isFinished()){
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 종료된 게임입니다.");
+            throw new GameFinishedException(gameId);
         }
 
 
